62f4749e48d5705dc1a44f916f94346b8cb530ec
kdtTetz <xenosign@naver.com>
2024-06-30 23:52:55 +0900

    240630 수업 준비

---
+++ src/lang/clazz/ClazzMain.java
@@ -0,0 +1,37 @@
+package lang.clazz;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ClazzMain {
+    public static void main(String[] args) throws Exception {
+        Class clazz1 = User.class; // 1. 클래스에서 직접 조회
+        Class clazz2 = new User("효석").getClass(); // 2. 인스턴스를 만들어서 조회
+        Class clazz3 = Class.forName("lang.clazz.User"); // 3. 문자열로 찾아서 조회
+
+        // 모든 필드 출력
+        Field[] fields = clazz1.getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println("field = " + field);
+        }
+
+        // 모든 메서드 출력
+        Method[] methods = clazz1.getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println("method = " + method);
+        }
+
+        // 상위 클래스 정보 출력
+        System.out.println("SuperClass = " + clazz1.getSuperclass());
+
+        // 인터페이스 정보 출력
+        Class[] interfaces = clazz1.getInterfaces();
+        for (Class inter : interfaces) {
+            System.out.println("inter = " + inter);
+        }
+
+        User user = (User) clazz1.getDeclaredConstructor().newInstance();
+        user.setName("효석");
+        System.out.println("user = " + user);
+    }
+}
---
+++ src/lang/math/RandomMain.java
@@ -0,0 +1,15 @@
+package lang.math;
+
+import java.util.Random;
+
+public class RandomMain {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        System.out.println("random.nextInt() = " + random.nextInt()); // int 범위의 +- 랜덤 정수
+        System.out.println("random.nextDouble() = " + random.nextDouble()); // 0 ~ 1.0 소수
+        System.out.println("random.nextBoolean() = " + random.nextBoolean()); // T, F 가 랜덤
+
+        System.out.println("random.nextInt(10) = " + random.nextInt(10)); // 0 ~ 9 까지의 정수        
+    }
+}
