48cf5d69aa018f7894b19e2bb5105d4ba635cf7c
kdtTetz <xenosign@naver.com>
2024-07-06 00:29:48 +0900

    240706 수업 준비

---
+++ src/generic/ex1/BoxMain3.java
@@ -0,0 +1,22 @@
+package generic.ex1;
+
+public class BoxMain3 {
+    public static void main(String[] args) {
+        GenericBox<Integer> integerGenericBox = new GenericBox<>();
+        integerGenericBox.setValue(10);
+        Integer integer = integerGenericBox.getValue();
+        System.out.println("integer = " + integer);
+        
+        GenericBox<String> stringGenericBox = new GenericBox<>();
+        stringGenericBox.setValue("hello");
+        String str = stringGenericBox.getValue();
+        System.out.println("str = " + str);
+
+        // 타입에 맞는 인수 전달
+        integerGenericBox.setValue(100);
+        // 타입에 안맞는 인수 전달
+        integerGenericBox.setValue("l00");
+
+        
+    }
+}
---
+++ src/generic/ex1/ObjectBox.java
@@ -0,0 +1,13 @@
+package generic.ex1;
+
+public class ObjectBox {
+    private Object obj;
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+}
