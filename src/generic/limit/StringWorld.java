36051b325f27b04cb5d97a6463c6ef4581e7430c
kdtTetz <xenosign@naver.com>
2024-07-07 14:19:40 +0900

    240707 수업 준비

---
+++ src/generic/limit/StringWorld.java
@@ -0,0 +1,26 @@
+package generic.limit;
+
+public class StringWorld {
+    private String str;
+
+    public StringWorld(String str) {
+        this.str = str;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public void 
+
+    @Override
+    public String toString() {
+        return "StringWorld{" +
+                "str='" + str + '\'' +
+                '}';
+    }
+}
